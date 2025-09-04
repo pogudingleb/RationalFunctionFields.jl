abf244ac476c8a19d5db4dd0e5d2056ed7b5d82e
pogudingleb <gleb.pogudin@polytechnique.edu>
2025-09-04 16:46:47 +0200

    code transfer, tests pass

--- src/RationalFunctionFields.jl
+++ src/RationalFunctionFields.jl
@@ -1,5 +1,35 @@
 module RationalFunctionFields
 
-# Write your package code here.
+# General packages
+using Combinatorics
+using LinearAlgebra
+using TimerOutputs
+
+# Algebra packages
+using AbstractAlgebra
+using Nemo
+using Groebner
+using ParamPunPam
+using ParamPunPam: reduce_mod_p!, specialize_mod_p, AbstractBlackboxIdeal
+ParamPunPam.enable_progressbar(false)
+
+
+export RationalFunctionField
+
+# membership functions
+export check_algebraicity, field_contains, issubfield, fields_equal
+export check_constructive_field_membership
+
+# simplification
+export simplified_generating_set 
+
+const _to = TimerOutputs.TimerOutput()
+
+include("util.jl")
+include("IdealMQS.jl")
+include("Field.jl")
+include("rankings.jl")
+include("normalforms.jl")
+include("constructive_membership.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
 using RationalFunctionFields
-using Test
 
-@testset "RationalFunctionFields.jl" begin
-    # Write your tests here.
+using Test
+using TestSetExtensions
+
+using Nemo
+
+function get_test_files()
+    result = Vector{String}()
+    for (dir, _, files) in walkdir("./")
+        for fname in files
+            if fname != "runtests.jl" && endswith(fname, ".jl")
+                push!(result, dir * "/" * fname)
+            end
+        end
+    end
+    return result
+end
+
+@info "Testing started"
+
+all_tests = get_test_files()
+if !isempty(ARGS)
+    all_tests = ARGS
+end
+
+@time @testset "All the tests" verbose = true begin
+    for test_file in all_tests
+            include(test_file)
+    end
 end
