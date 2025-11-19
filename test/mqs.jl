dff17a40437ede62c2bf56f1f41ab00d28c45e98
Sasha Demin <asdemin_2@edu.hse.ru>
2025-11-19 22:02:08 +0100

    compare specializations of generators

--- test/mqs.jl
+++ test/mqs.jl
@@ -17,4 +17,17 @@
         parent(t1)(0),
         t1*y2 - 1
     ]
+
+    K = Nemo.Native.GF(2^30+3)
+    RationalFunctionFields.reduce_mod_p!(mqs, K)
+    p = rand(K, length(params))
+    mqs_spec_mod_p = RationalFunctionFields.specialize_mod_p(mqs, p)
+    R = parent(mqs_spec_mod_p[1])
+    poly_mod_p(f, K) = map_coefficients(c -> K(numerator(c)) // K(denominator(c)), f)
+    gens_mod_p = map(f -> poly_mod_p(f, K), sys)
+    params_mod_p = map(f -> poly_mod_p(f, K), params)
+    indets_mod_p = map(f -> poly_mod_p(f, K), indets)
+    gens_spec_mod_p = map(f -> evaluate(f, params_mod_p, p), gens_mod_p)
+    gens_spec_mod_p = map(f -> RationalFunctionFields.parent_ring_change(f, R, matching=:byname), gens_spec_mod_p)
+    @test mqs_spec_mod_p == gens_spec_mod_p
 end
