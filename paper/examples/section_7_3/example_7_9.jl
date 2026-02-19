03702a0f86215812a050b0cfa28bc53f259a7b48
pogudingleb <gleb.pogudin@polytechnique.edu>
2026-02-19 11:59:23 +0100

    Updated examples 7_9 and 7_10

--- paper/examples/section_7_3/example_7_9.jl
+++ paper/examples/section_7_3/example_7_9.jl
@@ -1,31 +1,51 @@
 using Nemo
 using RationalFunctionFields
+using RationalFunctionFields: field_contains_mod_p
 
-R, (z1, z2, z3, z4, z5, z6, z7, z8, z9) =
-    polynomial_ring(QQ, ["z1", "z2", "z3", "z4", "z5", "z6", "z7", "z8", "z9"])
+R, (a1, a2, a3, b1, b2, b3) = polynomial_ring(QQ, ["a1", "a2", "a3", "b1", "b2", "b3"])
+
 
 gens = [
-    z5//z6,
-    1//z6*z4,
-    1//z9*z7,
-    1//z9*z8,
-    (z4*z9-z6*z7)//(z5*z9-z6*z8),
-    z6*(z1*z5*z9-z1*z6*z8-z2*z4*z9+z2*z6*z7+z3*z4*z8-z3*z5*z7)//(z5*z9-z6*z8),
-    (z1*z5*z9-z1*z6*z8-z2*z4*z9+z2*z6*z7+z3*z4*z8-z3*z5*z7)^2//z9//(z5*z9-z6*z8)^2,
-    z6*z9*(z4*z8-z5*z7)//(z1*z5*z9-z1*z6*z8-z2*z4*z9+z2*z6*z7+z3*z4*z8-z3*z5*z7),
-    z6*z9*(z5*z9-z6*z8)//(z1*z5*z9-z1*z6*z8-z2*z4*z9+z2*z6*z7+z3*z4*z8-z3*z5*z7),
-    -2*(z4*z8-z5*z7)//(z5*z9-z6*z8),
-    -(z4*z8-z5*z7)//(z5*z9-z6*z8),
-    2*(z4*z9-z6*z7)//(z5*z9-z6*z8),
-    -(z4*z8-z5*z7)^2//(z5*z9-z6*z8)^2,
-    -(z4*z9-z6*z7)^2//(z5*z9-z6*z8)^2,
-    2*(z4*z9-z6*z7)*(z4*z8-z5*z7)//(z5*z9-z6*z8)^2,
-    -z6*z9*(z4*z9-z6*z7)//(z1*z5*z9-z1*z6*z8-z2*z4*z9+z2*z6*z7+z3*z4*z8-z3*z5*z7),
+(a2-a3)*(a1-a3)*(a1-a2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(a1*b2^2-a1*b3^2-a2*b1^2+a2*b3^2+a3*b1^2-a3*b2^2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(a1*b1*b2-a1*b1*b3-a2*b1*b2+a2*b2*b3+a3*b1*b3-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(b1^2*b2+b1^2*b3+b1*b2^2-6*b1*b2*b3+b1*b3^2+b2^2*b3+b2*b3^2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(2*b1^3-b1^2*b2-b1^2*b3-b1*b2^2-b1*b3^2+2*b2^3-b2^2*b3-b2*b3^2+2*b3^3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(a1*a2*b1+a1*a2*b2-2*a1*a2*b3+a1*a3*b1-2*a1*a3*b2+a1*a3*b3-2*a2*a3*b1+a2*a3*b2+a2*a3*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(2*a1^2*b1-a1^2*b2-a1^2*b3-a2^2*b1+2*a2^2*b2-a2^2*b3-a3^2*b1-a3^2*b2+2*a3^2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(a1*b1^2*b2+a1*b1^2*b3-2*a1*b1*b2*b3+a2*b1*b2^2-2*a2*b1*b2*b3+a2*b2^2*b3-2*a3*b1*b2*b3+a3*b1*b3^2+a3*b2*b3^2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(a1*b1*b2-a1*b1*b3-a1*b2^2+a1*b3^2+a2*b1^2-a2*b1*b2+a2*b2*b3-a2*b3^2-a3*b1^2+a3*b1*b3+a3*b2^2-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(a1*b1*b2^2+a1*b1*b3^2-a1*b2^3-a1*b3^3-a2*b1^3+a2*b1^2*b2+a2*b2*b3^2-a2*b3^3-a3*b1^3+a3*b1^2*b3-a3*b2^3+a3*b2^2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(a1+a2+a3)*(a1*b1*b2-a1*b1*b3-a2*b1*b2+a2*b2*b3+a3*b1*b3-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(2*a1*b1^3-a1*b1^2*b2-a1*b1^2*b3+a1*b2^3-a1*b2^2*b3-a1*b2*b3^2+a1*b3^3+a2*b1^3-a2*b1^2*b3-a2*b1*b2^2-a2*b1*b3^2+2*a2*b2^3-a2*b2^2*b3+a2*b3^3+a3*b1^3-a3*b1^2*b2-a3*b1*b2^2-a3*b1*b3^2+a3*b2^3-a3*b2*b3^2+2*a3*b3^3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+(2*a1^2*b1^2+a1^2*b1*b2+a1^2*b1*b3-a1*a2*b1^2-a1*a2*b1*b3-a1*a2*b2^2-a1*a2*b2*b3-a1*a3*b1^2-a1*a3*b1*b2-a1*a3*b2*b3-a1*a3*b3^2+a2^2*b1*b2+2*a2^2*b2^2+a2^2*b2*b3-a2*a3*b1*b2-a2*a3*b1*b3-a2*a3*b2^2-a2*a3*b3^2+a3^2*b1*b3+a3^2*b2*b3+2*a3^2*b3^2)//(b3+b1+b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a2-a3)*(a1-a3)*(a1-a2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1*b2^2-a1*b3^2-a2*b1^2+a2*b3^2+a3*b1^2-a3*b2^2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-2*(a1*b1*b2-a1*b1*b3-a2*b1*b2+a2*b2*b3+a3*b1*b3-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1*b1*b2-a1*b1*b3-a2*b1*b2+a2*b2*b3+a3*b1*b3-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+2*(a1*b1*b2-a1*b1*b3-a2*b1*b2+a2*b2*b3+a3*b1*b3-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(b1^2*b2+b1^2*b3+b1*b2^2-6*b1*b2*b3+b1*b3^2+b2^2*b3+b2*b3^2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(2*b1^3-b1^2*b2-b1^2*b3-b1*b2^2-b1*b3^2+2*b2^3-b2^2*b3-b2*b3^2+2*b3^3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1^2*b1*b2+a1^2*b1*b3-2*a1*a2*b1*b2-2*a1*a3*b1*b3+a2^2*b1*b2+a2^2*b2*b3-2*a2*a3*b2*b3+a3^2*b1*b3+a3^2*b2*b3)//(b3+b1+b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1*b1*b2-a1*b1*b3-a1*b2^2+a1*b3^2+a2*b1^2-a2*b1*b2+a2*b2*b3-a2*b3^2-a3*b1^2+a3*b1*b3+a3*b2^2-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-1//2*(a1*b2+a1*b3+a2*b1+a2*b3+a3*b1+a3*b2)*(a1*b2-a1*b3-a2*b1+a2*b3+a3*b1-a3*b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+1//2*(a1*b2+a1*b3+a2*b1+a2*b3+a3*b1+a3*b2)*(a1*b2-a1*b3-a2*b1+a2*b3+a3*b1-a3*b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1+a2+a3)*(a1*b1*b2-a1*b1*b3-a2*b1*b2+a2*b2*b3+a3*b1*b3-a3*b2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1^2*a2*b2-a1^2*a2*b3-a1^2*a3*b2+a1^2*a3*b3+a1*a2^2*b1-a1*a2^2*b3+a1*a3^2*b1-a1*a3^2*b2-a2^2*a3*b1+a2^2*a3*b3-a2*a3^2*b1+a2*a3^2*b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1^2*a2*b1-a1^2*a2*b2+a1^2*a3*b1-a1^2*a3*b3-a1*a2^2*b1+a1*a2^2*b2-a1*a3^2*b1+a1*a3^2*b3+a2^2*a3*b2-a2^2*a3*b3-a2*a3^2*b2+a2*a3^2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-(a1*b1*b2^2-4*a1*b1*b2*b3+a1*b1*b3^2+a1*b2^2*b3+a1*b2*b3^2+a2*b1^2*b2+a2*b1^2*b3-4*a2*b1*b2*b3+a2*b1*b3^2+a2*b2*b3^2+a3*b1^2*b2+a3*b1^2*b3+a3*b1*b2^2-4*a3*b1*b2*b3+a3*b2^2*b3)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-1//2*(a1^2*b2^2-a1^2*b3^2-2*a1*a2*b1^2+2*a1*a2*b1*b3+2*a1*a2*b2^2-2*a1*a2*b2*b3+2*a1*a3*b1^2-2*a1*a3*b1*b2+2*a1*a3*b2*b3-2*a1*a3*b3^2-a2^2*b1^2+a2^2*b3^2+2*a2*a3*b1*b2-2*a2*a3*b1*b3-2*a2*a3*b2^2+2*a2*a3*b3^2+a3^2*b1^2-a3^2*b2^2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+1//2*(a1^2*b2^2-a1^2*b3^2-2*a1*a2*b1^2+2*a1*a2*b1*b3+2*a1*a2*b2^2-2*a1*a2*b2*b3+2*a1*a3*b1^2-2*a1*a3*b1*b2+2*a1*a3*b2*b3-2*a1*a3*b3^2-a2^2*b1^2+a2^2*b3^2+2*a2*a3*b1*b2-2*a2*a3*b1*b3-2*a2*a3*b2^2+2*a2*a3*b3^2+a3^2*b1^2-a3^2*b2^2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-1//2*(a1^2*b2^2+2*a1^2*b2*b3+a1^2*b3^2+2*a1*a2*b1*b2-2*a1*a2*b1*b3-2*a1*a2*b2*b3-2*a1*a2*b3^2-2*a1*a3*b1*b2+2*a1*a3*b1*b3-2*a1*a3*b2^2-2*a1*a3*b2*b3+a2^2*b1^2+2*a2^2*b1*b3+a2^2*b3^2-2*a2*a3*b1^2-2*a2*a3*b1*b2-2*a2*a3*b1*b3+2*a2*a3*b2*b3+a3^2*b1^2+2*a3^2*b1*b2+a3^2*b2^2)//(b3+b1+b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+-1//2*(a1^2*b2^2+2*a1^2*b2*b3+a1^2*b3^2-2*a1*a2*b1^2-2*a1*a2*b1*b2-2*a1*a2*b2^2+2*a1*a2*b3^2-2*a1*a3*b1^2-2*a1*a3*b1*b3+2*a1*a3*b2^2-2*a1*a3*b3^2+a2^2*b1^2+2*a2^2*b1*b3+a2^2*b3^2+2*a2*a3*b1^2-2*a2*a3*b2^2-2*a2*a3*b2*b3-2*a2*a3*b3^2+a3^2*b1^2+2*a3^2*b1*b2+a3^2*b2^2)//(b3+b1+b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+1//2*(2*a1^2*b1*b2+2*a1^2*b1*b3+a1^2*b2^2+2*a1^2*b2*b3+a1^2*b3^2-2*a1*a2*b1*b2-2*a1*a2*b1*b3-2*a1*a2*b2*b3-2*a1*a2*b3^2-2*a1*a3*b1*b2-2*a1*a3*b1*b3-2*a1*a3*b2^2-2*a1*a3*b2*b3+a2^2*b1^2+2*a2^2*b1*b2+2*a2^2*b1*b3+2*a2^2*b2*b3+a2^2*b3^2-2*a2*a3*b1^2-2*a2*a3*b1*b2-2*a2*a3*b1*b3-2*a2*a3*b2*b3+a3^2*b1^2+2*a3^2*b1*b2+2*a3^2*b1*b3+a3^2*b2^2+2*a3^2*b2*b3)//(b3+b1+b2)//(2*a1*b1-a1*b2-a1*b3-a2*b1+2*a2*b2-a2*b3-a3*b1-a3*b2+2*a3*b3),
+a1+a2+a3
 ]
 
-sg = simplified_generating_set(RationalFunctionField(gens))
-
-println("Simplified generators:")
-for f in sg
-    println(f)
+function td(f)
+    return total_degree(numerator(f // one(R))) + total_degree(denominator(f // one(R)))
 end
+
+println("There are $(length(gens)) original generators, their total degrees are $(map(td, gens))")
+sg = simplified_generating_set(RationalFunctionField(gens), enforce_minimality = true)
+println(sg)
