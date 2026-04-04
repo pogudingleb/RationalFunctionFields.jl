79e708edb85b56333b95ffe6e79760bbb8964060
Alexander Demin <asdemin_2@edu.hse.ru>
2026-04-04 10:18:49 +0300

    Trim blackbox cache assertions

--- ParamPunPam/test/blackbox.jl
+++ ParamPunPam/test/blackbox.jl
@@ -12,16 +12,9 @@ using Primes
     @test base_ring(bb) == Nemo.QQ
     K = Nemo.Native.GF(2^31 - 1)
     ParamPunPam.reduce_mod_p!(bb, K)
-    @test bb.small_prime == BigInt(characteristic(K))
-    @test !isempty(bb.polys_mod_small_p)
-    @test bb.large_prime == BigInt(0)
-    @test isempty(bb.polys_mod_large_p)
     point = map(K, [1])
     # @test ParamPunPam.specialize_mod_p(bb, point) == [2x + y, 2x + 1]
 
     Klarge = Nemo.GF(Primes.nextprime(big(2)^100))
     ParamPunPam.reduce_mod_p!(bb, Klarge)
-    @test bb.large_prime == BigInt(characteristic(Klarge))
-    @test !isempty(bb.polys_mod_small_p)
-    @test !isempty(bb.polys_mod_large_p)
 end
